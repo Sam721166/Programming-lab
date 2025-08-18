4783647ef9d7eee9a37cf5eeab6e6e691a6958b8
samiran <sam.de.721166@gmail.com>
2025-08-18 16:05:41 +0530

    Update assignment-8.c

--- 18 Aug/assignment-8.c
+++ 18 Aug/assignment-8.c
@@ -6,5 +6,12 @@ int main() {
     scanf("%f", &fahrenheit);
     celsius = (fahrenheit - 32) * 5 / 9;
     printf("Temperature in Celsius = %.2f\n", celsius);
+
+
+    float c2, f2;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &c2);
+    f2 = ((c2 * 9) / 5) + 32;
+    printf("Temperature in Fahrenheit = %.2f\n", f2);
     return 0;
 }
